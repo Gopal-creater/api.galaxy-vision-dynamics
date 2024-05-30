a8c096a2a3ef8b8d84a93dfa7e297be80a74af3d
gopal gautam <gopalgautam05@gmail.com>
2024-05-30 09:57:52 -0400

    Refined ajaya branch

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,9 +1,7 @@
-import factory from '../utils/handlerFactory.js';
-import { PrismaClient } from '@prisma/client';
+import prisma from "../config/db.config.js";
+import factory from "../utils/handlerFactory.js";
 
-const prisma = new PrismaClient();
-
-export const createArticle  = factory.createOne(prisma.articles);
+export const createArticle = factory.createOne(prisma.articles);
 export const deleteArticle = factory.deleteOne(prisma.articles);
 export const getAllArticles = factory.getAll(prisma.articles);
 export const getOneArticle = factory.getOne(prisma.articles);
